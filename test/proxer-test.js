f8d3ee6223814a19d09dea9701178246cdb7165e
Thiago Martins <tmartins@coddera.com>
2017-06-01 14:57:25 -0300

    Added logs to test

--- test/proxer-test.js
+++ test/proxer-test.js
@@ -7,7 +7,9 @@ describe("Proxer", _ => {
 
     it("Should proxy requests with body successfully", done => {
         let proxy = proxer.startNewProxy("httpbin.org", 9091);
-        fetch('http://localhost:9091/get?potato', {headers: {}}).then(res => res.json()).then(json => {
+        fetch('http://localhost:9091/get?potato', {headers: {}}).then(res => res.text())
+            .then(text => console.log(text))
+            .then(json => {
             let keys = Object.keys(json.args);
             expect(keys).to.include('potato');
             proxy.close();
