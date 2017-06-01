e71dd8cacd68ce654891b0b6fe65dd930e013804
Thiago Martins <tmartins@coddera.com>
2017-06-01 18:03:57 -0300

    Added tests for POSTS and Images

--- test/proxer-test.js
+++ test/proxer-test.js
@@ -1,5 +1,6 @@
 let expect = require('chai').expect,
     fetch = require('node-fetch'),
+    imageType = require('image-type'),
     proxer = require('../app/proxer');
 
 describe("Proxer", _ => {
@@ -19,8 +20,8 @@ describe("Proxer", _ => {
     it("Should start new proxy successfully", done => {
         let proxies = [
             proxer.startNewProxy("httpbin.org", 9091),
-            proxyB = proxer.startNewProxy("http://httpbin.org", 9092),
-            proxyC = proxer.startNewProxy("http://httpbin.org/", 9093),
+            proxer.startNewProxy("http://httpbin.org", 9092),
+            proxer.startNewProxy("http://httpbin.org/", 9093),
             //TODO: proxyD = proxer.startNewProxy("https://httpbin.org/", 9094)
         ];
 
@@ -35,5 +36,31 @@ describe("Proxer", _ => {
             proxies.forEach(it => it.close());
             done();
         })
-    })
+    });
+
+    it("Should proxy binary data successfully", done => {
+        let proxy = proxer.startNewProxy("httpbin.org", 9091);
+
+        fetch('http://localhost:9091/image/png').then(res => res.buffer())
+            .then(buffer => imageType(buffer))
+            .then(imgInfo => {
+                expect(imgInfo.mime).to.be.equal('image/png');
+                proxy.close();
+                done();
+            });
+    });
+
+    it("Should proxy POST requests successfully", done => {
+        let proxy = proxer.startNewProxy("httpbin.org", 8080);
+
+        let body = JSON.stringify({potatoes: 5});
+        let opts = {method: 'POST', body};
+        fetch('http://localhost:8080/post', opts).then(res => res.json())
+            .then(json => {
+                expect(json.data).to.be.equal(body);
+                proxy.close();
+                done();
+            });
+    });
+
 });
